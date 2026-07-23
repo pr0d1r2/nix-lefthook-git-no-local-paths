3f09be673c1df0d9d9efadcf67405c3685f84706
hallucinogen <hallucinogen@localhost>
2026-07-23 00:00:46 +0000

    chore: migrate vendored → referenced (set-and-setting standard)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Lefthook-compatible local filesystem path detector";
+  description = "CHANGEME";
 
   nixConfig = {
     extra-substituters = [ "https://pr0d1r2.cachix.org" ];
@@ -11,129 +11,25 @@
     nixpkgs.follows = "nixpkgs-lock/nixpkgs";
 
     set-and-setting.url = "github:pr0d1r2/set-and-setting";
-    set-and-setting.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-lefthook-nixfmt-src = {
-      url = "github:pr0d1r2/nix-lefthook-nixfmt";
-      flake = false;
-    };
-    nix-lefthook-shellcheck-src = {
-      url = "github:pr0d1r2/nix-lefthook-shellcheck";
-      flake = false;
-    };
-    nix-lefthook-shfmt-src = {
-      url = "github:pr0d1r2/nix-lefthook-shfmt";
-      flake = false;
-    };
-    nix-lefthook-statix-src = {
-      url = "github:pr0d1r2/nix-lefthook-statix";
-      flake = false;
-    };
-    nix-lefthook-deadnix-src = {
-      url = "github:pr0d1r2/nix-lefthook-deadnix";
-      flake = false;
-    };
-    nix-lefthook-bats-parse-src = {
-      url = "github:pr0d1r2/nix-lefthook-bats-parse";
-      flake = false;
-    };
-    nix-lefthook-bats-unit-src = {
-      url = "github:pr0d1r2/nix-lefthook-bats-unit";
-      flake = false;
-    };
-    nix-lefthook-yamllint-src = {
-      url = "github:pr0d1r2/nix-lefthook-yamllint";
-      flake = false;
-    };
-    nix-lefthook-typos-src = {
-      url = "github:pr0d1r2/nix-lefthook-typos";
-      flake = false;
-    };
-    nix-lefthook-editorconfig-checker-src = {
-      url = "github:pr0d1r2/nix-lefthook-editorconfig-checker";
-      flake = false;
-    };
-    nix-lefthook-markdownlint-src = {
-      url = "github:pr0d1r2/nix-lefthook-markdownlint";
-      flake = false;
-    };
-    nix-lefthook-markdownlint-agentic-src = {
-      url = "github:pr0d1r2/nix-lefthook-markdownlint-agentic";
-      flake = false;
-    };
-    nix-lefthook-git-conflict-markers-src = {
-      url = "github:pr0d1r2/nix-lefthook-git-conflict-markers";
-      flake = false;
-    };
-    nix-lefthook-missing-final-newline-src = {
-      url = "github:pr0d1r2/nix-lefthook-missing-final-newline";
-      flake = false;
-    };
-    nix-lefthook-trailing-whitespace-src = {
-      url = "github:pr0d1r2/nix-lefthook-trailing-whitespace";
-      flake = false;
-    };
-    nix-lefthook-nix-no-embedded-shell-src = {
-      url = "github:pr0d1r2/nix-lefthook-nix-no-embedded-shell";
-      flake = false;
-    };
-    nix-lefthook-file-size-check-src = {
-      url = "github:pr0d1r2/nix-lefthook-file-size-check";
-      flake = false;
-    };
   };
 
   outputs =
-    inputs@{
+    {
       self,
       nixpkgs,
       set-and-setting,
       ...
     }:
-    let
-      supportedSystems = import ./nix/systems.nix;
-      forAllSystems =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
-
-      batsWithLibsFor = import ./nix/bats-with-libs.nix;
-    in
-    {
-      packages = forAllSystems (pkgs: {
-        default = import ./nix/package.nix pkgs;
-      });
-
-      checks = import ./nix/checks.nix forAllSystems set-and-setting ./.;
-
-      devShells = forAllSystems (
-        pkgs:
-        let
-          inherit (pkgs.stdenv.hostPlatform) system;
-          batsWithLibs = batsWithLibsFor pkgs;
-          ciPackages = [
-            self.packages.${system}.default
-          ]
-          ++ (import ./nix/lefthook-wrappers.nix { inherit pkgs inputs; })
-          ++ [
-            pkgs.coreutils
-            pkgs.git
-            pkgs.lefthook
-            pkgs.nix
-            pkgs.taplo
-            batsWithLibs
-          ];
-        in
-        {
-          ci = pkgs.mkShell {
-            packages = ciPackages;
-            BATS_LIB_PATH = "${batsWithLibs}/share/bats";
-          };
-          default = pkgs.mkShell {
-            packages = ciPackages ++ [ pkgs.gh ];
-            shellHook = builtins.replaceStrings [ "@BATS_LIB_PATH@" ] [ "${batsWithLibs}" ] (
-              builtins.readFile ./dev.sh
-            );
-          };
-        }
-      );
+    set-and-setting.lib.mkConsumerFlake {
+      inherit self nixpkgs set-and-setting;
+      fragments = [
+        "base"
+        "nix"
+        "shell"
+        "ascii"
+        "markdown"
+        "yaml"
+      ];
+      src = ./.;
     };
 }
